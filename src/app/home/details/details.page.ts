127ece76b36562426c07718450a27adf99a9e7cb
miiakate <ekaterina@witchery.io>
2021-05-01 23:00:27 +0400

    a

--- src/app/home/details/details.page.ts
+++ src/app/home/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  public value;
 
+  constructor() {
+
+  }
   ngOnInit() {
+    console.log(this.value.bank_branches)
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { ModalController, NavController } from '@ionic/angular';
+import { DetailsPage } from './details/details.page';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  configUrl = 'http://localhost:3000/banks';
+  banks: any;
+  constructor(private http: HttpClient,
+    private modalController: ModalController,
+    private navCtrl: NavController) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): any {
+    return this.http.get(this.configUrl).subscribe((data) => {
+      this.banks = data;
+      console.log(data);
+    });
+  }
+  async showBank(bank) {
+    // console.log(bank)
+    const modal = await this.modalController.create({
+      component: DetailsPage,
+      componentProps: { value: bank }
+    });
+    return await modal.present();
   }
 
 }
